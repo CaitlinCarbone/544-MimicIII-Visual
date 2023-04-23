881a4579df5baf43aa14fe29f9b8b9418674797a
Caitlin <caitlintcarbone@gmail.com>
2023-04-23 03:19:01 -0400

    added meat to the website

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('static'))
+
 // app.use(express.static("static"));
 
 // Our implemented routes
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,21 @@ const constructorMethod = (app) => {
   app.get("/home", (req, res) => {
     res.sendFile(path.resolve("static/homepage.html"));
   });
+  app.get("/sepsis", (req, res) => {
+    res.sendFile(path.resolve("static/sepsis.html"));
+  });
+  app.get("/pneumonia", (req, res) => {
+    res.sendFile(path.resolve("static/pneumonia.html"));
+  });
+  app.get("/intracranialhemorrhage", (req, res) => {
+    res.sendFile(path.resolve("static/intracranialhemorrhage.html"));
+  });
+  app.get("/congestiveheartfailure", (req, res) => {
+    res.sendFile(path.resolve("static/congestiveheartfailure.html"));
+  });
+  app.get("/alteredmentalstatus", (req, res) => {
+    res.sendFile(path.resolve("static/alteredmentalstatus.html"));
+  });
   app.get("/error", (req, res) => {
     res.sendFile(path.resolve("static/errorPage.html"));
   });
